32c9f6152d10ae2852df6bd3fb11d09b3f1e6747
Aleks Petrov <aleks0317p@gmail.com>
2022-06-02 20:34:03 +0200

    Начало 3 части

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -14,6 +14,7 @@ func main() {
 	config, _ := apiserver.ParseBaseConfig("C:/Users/Admin/go/src/github.com/ALPetrov/my-rest-api/configs/apiserver.json")
 
 	s := apiserver.New(config)           //передаем config в качестве аргумента
+	
 	if err := s.Start(); err != nil {    //вызываем функц Старт и запускаем его
 		log.Fatal(err)                  // если что- то не так, выводит ошибку
 	}
